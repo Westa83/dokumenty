40d155ae4acd2fc90b54776100f50870159dfa0f
Aga <agwe@onet.eu>
2020-08-08 23:29:58 +0200

    nowy

---
+++ test.py
@@ -0,0 +1,32 @@
+print('No to gramy !!!!')
+
+print('Podaj pierwsza liczbę')
+number1 = input()
+
+print('Podaj drugą liczbę')
+number2 = input()
+
+print(f'ile to jest: {number1} + {number2}')
+result = input()
+
+print(f'jesteś pewna że to: {result}')
+answer = input()
+
+
+if answer == 'tak' and (number1 + number2) == answer:
+    print('BRAWO')
+elif answer == 'nie' and (number1 + number2) != answer:
+    print('MIALAS RACJE')
+else:
+    print('PUDLO !!!!!')
+
+
+
+
+
+
+
+# bo powinno byc {number1 + liczba2}
+
+
+
